0446f40635394011f8d04b9cd505b9aa80e17197
prakyathshetty25 <prakyathashetty@gmail.com>
2024-11-04 10:02:13 +0530

    commit

--- server.js
+++ server.js
@@ -1,32 +1,79 @@
-// server.js
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const path = require('path');
+const User = require('./models/User');
 
 const app = express();
 const PORT = 3000;
 
-// Set EJS as the templating engine
-app.set('view engine', 'ejs');
-
-// Set the views directory
-app.set('views', path.join(__dirname, 'views'));
-
-// Define a route for the root URL
-app.get('/', (req, res) => {
-    res.send('index.ejs'); // Simple text response
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/loginSignupExample', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
 });
 
-// Alternatively, if you want to render an EJS file
-// app.get('/', (req, res) => {
-//     res.render('index'); // Make sure you have an index.ejs file in the views folder
-// });
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// Define a route for the login page
+// Middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Route for the home page
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Route to render the login page
 app.get('/login', (req, res) => {
-    res.render('login'); // This will render views/login.ejs
+    res.render('login');
+});
+
+// Route to render the signup page
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+// Handle signup form submission
+app.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser  = new User({ username, password: hashedPassword });
+    await newUser .save();
+
+    res.send(`Signup successful for ${username}!`);
+});
+
+// Handle login form submission
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Find the user by username
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.send('Invalid username or password.');
+    }
+
+    // Check the password
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+        res.send('Login successful!');
+    } else {
+        res.send('Invalid username or password.');
+    }
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
